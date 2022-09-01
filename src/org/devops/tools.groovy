9e7b4a8158b0f0cc0728fe0cbbcc2ab3cb04a2a1
kyriefly <66997396+kyriefly@users.noreply.github.com>
2022-09-01 01:18:40 -0700

    Update tools.groovy

--- src/org/devops/tools.groovy
+++ src/org/devops/tools.groovy
@@ -1,6 +1,26 @@
 package org.devops
 
-//打印内容
-def printMes(content){
-    println(content)
+//格式化输出
+def PrintMes(value,color){
+    colors = ['red'   : "\033[40;31m >>>>>>>>>>>${value}<<<<<<<<<<< \033[0m",
+              'blue'  : "\033[47;34m ${value} \033[0m",
+              'green' : "[1;32m>>>>>>>>>>${value}>>>>>>>>>>[m",
+              'green1' : "\033[40;32m >>>>>>>>>>>${value}<<<<<<<<<<< \033[0m" ]
+    ansiColor('xterm') {
+        println(colors[color])
+    }
+}
+
+
+// 获取镜像版本
+def createVersion() {
+	// 定义一个版本号作为当次构建的版本，输出结果 20191210175842_69
+    return new Date().format('yyyyMMddHHmmss') + "_${env.BUILD_ID}"
+}
+
+
+// 获取时间
+def getTime() {
+	// 定义一个版本号作为当次构建的版本，输出结果 20191210175842
+    return new Date().format('yyyyMMddHHmmss')
 }
